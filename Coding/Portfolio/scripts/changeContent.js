14f803f71c8ba86b2bd4f29262c0353995cf53a1
AaronGSM5 <140388648+AaronGSM5@users.noreply.github.com>
2023-11-20 12:43:07 +0100

    All Learning Projects

---
+++ Coding/Portfolio/scripts/changeContent.js
@@ -0,0 +1,23 @@
+document.querySelector('.about').addEventListener('click', () => 
+{
+    let content = document.querySelector('.content') 
+
+    content.innerHTML =
+    ''
+})
+
+document.querySelector('.work').addEventListener('click', () => 
+{
+    let content = document.querySelector('.content') 
+
+    content.innerHTML =
+    ''
+})
+
+document.querySelector('.contact').addEventListener('click', () => 
+{
+    let content = document.querySelector('.content') 
+
+    content.innerHTML =
+    ''
+})
