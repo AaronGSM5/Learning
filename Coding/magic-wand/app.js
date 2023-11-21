d3804703ca29f574bc846920f02228505611cd08
AaronGSM5 <140388648+AaronGSM5@users.noreply.github.com>
2023-11-21 20:47:46 +0100

    wandrework

--- Coding/magic-wand/app.js
+++ Coding/magic-wand/app.js
@@ -3,27 +3,29 @@ const blur = document.querySelector('.blur')
 const wrapper = document.querySelector('.window')
 //Detect touch device
 
-function move(e) {
-  //PageX and PageY return the position of users cursor from top left of screen
-  var x = e.pageX  * 100 / window.innerWidth
-  var y = e.pageY  * 100 / window.innerHeight
-  console.log(x)
-
-  wand.style.transform = `rotate(${x/5}deg)`
-
-
-  //set left and top of div based on mouse position
-  wand.style.left = x   + "%";
-  wand.style.top = y   + "%";
-
-  if (x > 20 && x < 80 && y < 75 ) {
-    blur.style.backdropFilter = `blur(${x * 2/10-4}px)`
-  }
-
+wrapper.addEventListener("mousemove", (e) => {
+    //PageX and PageY return the position of users cursor from top left of screen
+    const x = (e.pageX - e.currentTarget.offsetLeft) / e.currentTarget.offsetWidth * 100
+    const y = (e.pageY - e.currentTarget.offsetTop) / e.currentTarget.offsetHeight * 100
   
-};
-//For mouse
-document.addEventListener("mousemove", (e) => {
-  move(e);
+    var wandX = (x * -0.15) + x * 1.15
+    var wandY = (y * -0.5) + y * 1.15
+  
+  
+    wand.animate({
+      left: `${wandX}%`,
+      top: `${wandY}%`,
+      rotate: `${-5+ (10 * x / 100)}deg`
+    }, {duration: 400, fill:"forwards"})
+  
+  
+    //set left and top of div based on mouse position
+  
+  
+      blur.style.backdropFilter = `blur(${(x * 2/10-4)}px)`
+
+      //CHange blur
+      //Edit position of wand
+
 });
 
