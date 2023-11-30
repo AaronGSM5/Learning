528f1fcdc9af9a7dc97fe40d38a4bea012618ed0
AaronGSM5 <140388648+AaronGSM5@users.noreply.github.com>
2023-11-30 23:55:56 +0100

    newsliding

--- Coding/cool-hover/app.js
+++ Coding/cool-hover/app.js
@@ -5,20 +5,20 @@ const slides = document.getElementsByClassName("slides");
 
 function like() {
   if (enableButtons) {
+    enableButtons = false;
+
     const nextIndex = activeIndex + 1 <= slides.length -1 ? activeIndex + 1 : 0;
 
     const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`)
     const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
-  
+    
+    nextSlide.style = ''
     currentSlide.dataset.status = "after";
     nextSlide.dataset.status = "active";
-    
+    randomDirection();
+
     setTimeout(() => {
       currentSlide.dataset.status = "unknown";
-    }, 500);
-
-    enableButtons = false;
-    setTimeout(() => {
       enableButtons = true;
     }, 800);
   
@@ -26,25 +26,41 @@ function like() {
   }
 }
 
+
 function dislike() {
   if (enableButtons) {
+    enableButtons = false;
+
     const nextIndex = activeIndex + 1 <= slides.length -1 ? activeIndex + 1 : 0;
   
     const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`)
     const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
-  
+    
+    nextSlide.style = ''
     currentSlide.dataset.status = "before";
     nextSlide.dataset.status = "active";
-  
-    setTimeout(() => {
-      currentSlide.dataset.status = "unknown";
-    }, 500);
-  
-    enableButtons = false;
+    randomDirection();
+
     setTimeout(() => {
       enableButtons = true;
+      currentSlide.dataset.status = "unknown";
     }, 800);
   
     activeIndex = nextIndex;
   }
+}
+
+function randomDirection() {
+  const random = Math.round(45 - (Math.random() * 90))
+
+  try {
+    document.querySelector(`[data-status="before"]`).style = `rotate: ${random}deg`
+  } 
+  catch (error) {
+    document.querySelector(`[data-status="after"]`).style = `rotate: ${random}deg`
+  }
+
+
+  // document.querySelector('[data-status="after"]').style.rotate = random;
+
 }
