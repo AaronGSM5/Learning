eefb9d5ee47e6ef54d8a1800dfb67f80b6e6099c
AaronGSM5 <140388648+AaronGSM5@users.noreply.github.com>
2023-12-01 13:29:42 +0100

    pythoncourse

--- Coding/cool-hover/app.js
+++ Coding/cool-hover/app.js
@@ -3,7 +3,7 @@ let enableButtons = true;
 
 const slides = document.getElementsByClassName("slides");
 
-function like() {
+function likeOrDislike(whichOne) {
   if (enableButtons) {
     enableButtons = false;
 
@@ -13,37 +13,13 @@ function like() {
     const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
     
     nextSlide.style = ''
-    currentSlide.dataset.status = "after";
+    currentSlide.dataset.status = `${whichOne}`;
     nextSlide.dataset.status = "active";
     randomDirection();
 
     setTimeout(() => {
       currentSlide.dataset.status = "unknown";
       enableButtons = true;
-    }, 800);
-  
-    activeIndex = nextIndex;
-  }
-}
-
-
-function dislike() {
-  if (enableButtons) {
-    enableButtons = false;
-
-    const nextIndex = activeIndex + 1 <= slides.length -1 ? activeIndex + 1 : 0;
-  
-    const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`)
-    const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
-    
-    nextSlide.style = ''
-    currentSlide.dataset.status = "before";
-    nextSlide.dataset.status = "active";
-    randomDirection();
-
-    setTimeout(() => {
-      enableButtons = true;
-      currentSlide.dataset.status = "unknown";
     }, 800);
   
     activeIndex = nextIndex;
